818d3481868db83bec463bac9663749a3febceb7
yusei <iwaitforthree2324@gmail.com>
2026-01-02 01:35:32 +0900

    Fix: Serverless Functiion 3

--- src/api/getIss.ts
+++ src/api/getIss.ts
@@ -1,13 +1,11 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(
-  _req: VercelRequest,
-  res: VercelResponse
-) {
+// src/api/getIss.ts
+export default async function handler(_req: any, res: any) {
   try {
     const response = await fetch("http://api.open-notify.org/iss-now.json");
     const data = await response.json();
-    res.setHeader("Access-Control-Allow-Origin", "*"); // CORS対応
+
+    // フロントに返す
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch {
     res.status(500).json({ error: "取得失敗" });
--- src/api/iss.ts
+++ src/api/iss.ts
@@ -1,7 +1,7 @@
 import type { IssPositionResponse } from "../types/iss";
 
 export async function fetchIssPosition(): Promise<IssPositionResponse>{
-    const res = await fetch('http://api.open-notify.org/iss-now.json');
+    const res = await fetch('/api/getIss');
     if(!res.ok){
         throw new Error('ISSの位置情報を取得できませんでした');
     }
